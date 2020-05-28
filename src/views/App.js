67f066200ac5286c7f60d22ef7bf9094e6977d90
qiaowenwen3 <qiaowenwen@eastmoney.com>
2020-05-28 19:30:09 +0800

    代办事项

--- src/views/App.js
+++ src/views/App.js
@@ -4,7 +4,13 @@ function App() {
   const [data, setData] = useState([
     { name: '图片上传', url: '/ReactMobileH5/upload' },
     { name: '图片加载', url: '/ReactMobileH5/animation' },
-    { name: '日历', url: '/ReactMobileH5/calendar' }
+    { name: '日历', url: '/ReactMobileH5/calendar' },
+    { name: '弹窗', url: '/ReactMobileH5/calendar' },
+    { name: '固定的表格', url: '/ReactMobileH5/calendar' },
+    { name: '数字键盘', url: '/ReactMobileH5/calendar' },
+    { name: '轮播', url: '/ReactMobileH5/calendar' },
+    { name: '分页', url: '/ReactMobileH5/calendar' },
+    { name: '表单', url: '/ReactMobileH5/calendar' }
   ])
   const [flag, setflag] = useState('1')
 
